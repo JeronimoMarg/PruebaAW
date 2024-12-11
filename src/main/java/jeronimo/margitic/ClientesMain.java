c0b30aca4c3196822b587f4914dc283a48a1b297
JeronimoMarg <jeronimomargitic@gmail.com>
2024-12-10 23:22:02 -0300

    Tweaks para conexion con BD

--- src/main/java/jeronimo/margitic/ClientesMain.java
+++ src/main/java/jeronimo/margitic/ClientesMain.java
@@ -1,4 +1,4 @@
-package jeronimo.margitic.microservice_clientes;
+package jeronimo.margitic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
